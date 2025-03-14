e76df64964e07ed593791b87465a498e69e21a45
Saikat Halder <saikat.halder@lotto24.de>
2025-03-14 10:17:00 +0100

    add Signal class and createEffect function

--- src/signal/signal.ts
+++ src/signal/signal.ts
@@ -1,3 +1,36 @@
-// First we need to create the signal class
+let effectCallback: (() => void) | null = null
 
-// Create the effect function
+export class Signal<T> {
+    private value: T
+    private subscribers: ((value: T) => void)[] = []
+
+    constructor(value: T) {
+        this.value = value
+        this.subscribers = []
+    }
+
+    subscribe(subscriber: (value: T) => void) {
+        this.subscribers.push(subscriber)
+    }
+
+    set(value: T) {
+        if (this.value === value) {
+            return
+        }
+        this.value = value
+        this.subscribers.forEach(subscriber => subscriber(this.value))
+    }
+
+    get() {
+        if (effectCallback) {
+            this.subscribe(effectCallback)
+        }
+        return this.value
+    }
+}
+
+export const createEffect = (effect: () => void) => {
+    effectCallback = effect
+    effect()
+    effectCallback = null
+}
